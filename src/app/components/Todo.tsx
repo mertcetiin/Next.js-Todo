29437f53c077f0b074365bb559085680f962e5a9
mertcetiin <mrtctn506@gmail.com>
2023-11-24 18:16:15 +0300

    components added

---
+++ src/app/components/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            <input type="text" className='pl-2 ml-5 rounded outline-none' />
+            <button className='ml-5 border-2 p-2 mt-10 rounded w-22 bg-blue-200 outline-none'>Add</button>
+        </div>
+    )
+}
+
+export default Todo;
