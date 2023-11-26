c749a35ab42018928436c5dc0253e9242e72d2be
mertcetiin <mrtctn506@gmail.com>
2023-11-27 00:50:37 +0300

    todo finished

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { useTodoStore } from '../states/store';
+"use client";
+import { useTodoStore } from "../states/store";
 
 
 function Todo() {
 
+    const inputValue = useTodoStore((state) => state.inputValue);
+    const todoState = useTodoStore((state) => state.todoState);
+    const newTodoState = useTodoStore((state) => state.newTodoState);
+    const addTodo = useTodoStore((state) => state.addTodo);
 
 
     return (
         <div>
-            <input type="text" className='pl-2 ml-5 rounded outline-none' />
-            <button className='ml-5 border-2 p-2 mt-10 rounded w-22 bg-blue-200 outline-none'>Add</button>
-        </div>
+            <input type="text" className='pl-2 ml-5 rounded outline-none'
+                name='name'
+                value={newTodoState}
+                onChange={(e) => inputValue(e.target.value)}
+            />
+            <button className='ml-5 border-2 p-2 mt-10 rounded w-22 bg-blue-200 outline-none'
+                onClick={addTodo}
+            >Add</button>
+
+            {todoState.map((item: string, id: number) => (
+                <div key={id}>
+                    <h1>{item}</h1>
+                </div>
+            ))}
+
+        </div >
     )
 }
 
--- src/app/states/store.tsx
+++ src/app/states/store.tsx
@@ -1,9 +1,20 @@
 import { create } from "zustand";
 
 interface TodoState {
-
+    inputValue: (value: string) => void;
+    todoState: any;
+    newTodoState: any;
+    addTodo: () => void;
 }
 
 export const useTodoStore = create<TodoState>()((set) => ({
+    todoState: [],
+    newTodoState: '',
 
-}))
+    inputValue: (value => set({ newTodoState: value })),
+
+    addTodo: () => set((state) => ({
+        todoState: [...state.todoState, state.newTodoState],
+        newTodoState: '',
+    })),
+}));
